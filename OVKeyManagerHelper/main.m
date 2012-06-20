d376d211f22f25487340f4e6495246b920e1eb7f
Barry Wark <barrywark@gmail.com>
2012-06-20 12:04:52 -0400

    Info and Launchd plists for helper

--- OVKeyManagerHelper/main.m
+++ OVKeyManagerHelper/main.m
@@ -3,7 +3,7 @@
 //  OVKeyManagerHelper
 //
 //  Created by Barry Wark on 6/20/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 Physion Consulting LLC. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
